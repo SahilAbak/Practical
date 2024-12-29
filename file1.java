2487c516bedde3abc40a6cb7b62e10c59573939c
SahilAbak <sahilabak45@gmail.com>
2024-12-29 20:08:41 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -1,4 +1,6 @@
 public class file1{
 public static void main(String[] args){
-System.out.println("Krushna yadd zava aahe");
+System.out.println("Krushna yadd zava aahe ");
+  System.out.println("kharach aahe");
+
 }}
